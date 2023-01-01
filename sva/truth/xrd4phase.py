cb893129be3aafafd7bd3a383ddad7e27920cdc8
Matthew Carbone <x94carbone@gmail.com>
2022-12-31 21:56:41 -0500

    Revise the xrd4phase example

--- sva/truth/xrd4phase.py
+++ sva/truth/xrd4phase.py
@@ -7,21 +7,20 @@ import pandas as pd
 from sva.truth.common import sigmoid
 
 
-def theta_phase(x, y, x0=1, a=10):
-    # Radially symmetric about theta=0
-    # x, y = scale_coords(x, y)
-    angle = np.arctan2(y - 0.5, x - 0.5)
-    return 1.0 - sigmoid(np.abs(angle), x0=x0, a=a)
+def slow_linear_phase(x, y, x0=0.15, a=10.0):
+    # y = -x + 1
+    d = np.abs(y - (1.25 - 2.0 * x))
+    return 1.0 - sigmoid(d, x0=x0, a=a)
 
 
-def corner_phase(x, y, x0=0.25, a=20, loc_x=0.0, loc_y=1.0):
+def corner_circle_phase(x, y, x0=0.5, a=30.0, loc_x=1.0, loc_y=1.0):
     # Distance from the top left corner
     # x, y = scale_coords(x, y)
     r = np.sqrt((loc_x - x) ** 2 + (loc_y - y) ** 2)
     return 1.0 - sigmoid(r, x0=x0, a=a)
 
 
-def circle_phase(x, y, x0=0.5, a=20, loc_x=0.125, loc_y=0.125):
+def side_circle_phase(x, y, x0=0.2, a=50.0, loc_x=0.0, loc_y=0.25):
     # Distance from a point near the bottom right quadrant
     # x, y = scale_coords(x, y)
     r = np.sqrt((loc_x - x) ** 2 + (loc_y - y) ** 2)
@@ -42,9 +41,9 @@ def _get_xrd_map():
 def truth_xrd4phase(X):
     pure_phases = _get_xrd_map()
     # Gets the actual "value" of the observation
-    prop2 = theta_phase(X[:, 0], X[:, 1], x0=0.5, a=5.0)
-    prop1 = corner_phase(X[:, 0], X[:, 1], x0=0.5, a=30.0)
-    prop3 = circle_phase(X[:, 0], X[:, 1], x0=0.05, a=50.0)
+    prop1 = corner_circle_phase(X[:, 0], X[:, 1])
+    prop2 = slow_linear_phase(X[:, 0], X[:, 1])
+    prop3 = side_circle_phase(X[:, 0], X[:, 1])
     total_prop = prop1 + prop2 + prop3
     total_prop[total_prop > 1.0] = 1.0
     prop4 = 1.0 - total_prop

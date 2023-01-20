5938a3d4b9e9c5dd9a72a29005d6529ad429875f
Matthew Carbone <x94carbone@gmail.com>
2023-01-20 13:24:12 -0500

    Add qol code for metrics to bto

--- sva/truth/bto.py
+++ sva/truth/bto.py
@@ -4,6 +4,7 @@ from pathlib import Path
 import numpy as np
 import xarray as xr
 from scipy.interpolate import interp1d
+from tqdm import tqdm
 
 
 @cache
@@ -67,7 +68,7 @@ def _get_cmf_predicted_phase_fractions_linspace(X: np.ndarray):
     return weights.interp(temperature=X)
 
 
-def cmf_predicted_mse(X, linspace_points=5_000):
+def cmf_predicted_mse(X, linspace_points=5_000, use_only=None):
     """Get the mean squared error between the set of queries and a linspace,
     treating the CMF data for weights are ground truth for phase fractions.
 
@@ -91,4 +92,86 @@ def cmf_predicted_mse(X, linspace_points=5_000):
         fill_value=(known_weights[:, 0], known_weights[:, -1]),
     )
     interpolated_weights = f(linspace)
-    return np.nanmean((true_weights - interpolated_weights) ** 2)
+
+    tmp = (true_weights - interpolated_weights) ** 2
+    if use_only is not None:
+        tmp = tmp[:, use_only]
+
+    return np.nanmean(tmp)
+
+
+def bto_compute_metrics_all_acquisition_functions_and_LTB(
+    results_by_acqf,
+    metrics_grid=list(range(3, 251, 10)),
+    metrics_grid_linear=list(range(3, 251, 10)),
+    metric="mse",
+    grid_points=10000,
+    disable_pbar=False,
+    xmin=150.0,
+    xmax=445.0,
+):
+    """Computes the metric provided across all data.
+
+    Parameters
+    ----------
+    results_by_acqf : dict
+        Result of ``sva.postprocessing.parse_results_by_acquisition_function``.
+    metrics_grid : array_like, optional
+        The n-grid for the metric.
+    metrics_grid_linear : array_like, optional
+        The n-grid for the LTB metric.
+    metric : {"mse", "rmae"}, optional
+    grid_points : int, optional
+    interpolation_method : str, optional
+    disable_pbar : bool, optional
+
+    Returns
+    -------
+    dict
+    """
+
+    if metric == "mse":
+        metric_function = cmf_predicted_mse
+    else:
+        raise ValueError(f"Unknown metric={metric}")
+
+    G = np.linspace(150.0, 445.0, grid_points)
+    if xmin == 150.0 and xmax == 445.0:
+        use_only = None
+    else:
+        use_only = np.where((G <= xmax) & (G >= xmin))[0]
+
+    all_metrics = dict()
+    for acquisition_function_name, values in results_by_acqf.items():
+        tmp_metrics = []
+        for n in tqdm(metrics_grid, disable=disable_pbar):
+            tmp_list_1 = []
+            for exp in values:
+                X_tmp = exp.data.X[:n].squeeze()
+                try:
+                    r = metric_function(
+                        X_tmp,
+                        linspace_points=grid_points,
+                        use_only=use_only,
+                    )
+                except IndexError:
+                    r = np.nan
+                tmp_list_1.append(r)
+            tmp_metrics.append(tmp_list_1)
+        all_metrics[acquisition_function_name] = np.array(tmp_metrics)
+
+    all_metrics["Linear"] = []
+    for N in tqdm(metrics_grid_linear, disable=disable_pbar):
+        res = metric_function(
+            np.linspace(150.0, 445.0, N),
+            linspace_points=grid_points,
+            use_only=use_only,
+        )
+        all_metrics["Linear"].append(res)
+    all_metrics["Linear"] = np.array(all_metrics["Linear"]).reshape(-1, 1)
+
+    return {
+        "metrics": all_metrics,
+        "metrics_grid": metrics_grid,
+        "metrics_grid_linear": metrics_grid_linear,
+    }

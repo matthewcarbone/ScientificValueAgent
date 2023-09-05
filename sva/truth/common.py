26887a19603a53c63ed2be76e90774ad293a6937
Matthew Carbone <x94carbone@gmail.com>
2023-09-05 11:25:05 -0400

    Add random sampling code

--- sva/truth/common.py
+++ sva/truth/common.py
@@ -61,7 +61,8 @@ def sigmoid(x, x0, a):
 
 
 def limited_time_budget(N, dims):
-    """Returns the grid of the limited time budget (LTB) "policy".
+    """Returns the grid of the limited time budget (LTB) "policy". Points are
+    returned in the range [0, 1).
 
     Parameters
     ----------
@@ -78,6 +79,26 @@ def limited_time_budget(N, dims):
     deltax = 1.0 / (N - 1)
     arr = [deltax * ii for ii in range(N)]
     return np.array(list(product(*[arr for _ in range(dims)])))
+
+
+def random_sampling(N, dims):
+    """Returns a random sampling of the space. Points are returned in the range
+    [0, 1).
+
+    Parameters
+    ----------
+    N : int
+        The number of points to sample _per dimension_.
+    dims : int
+        The number of dimensions.
+
+    Returns
+    -------
+    np.ndarray
+    """
+
+    total_points = N**dims
+    return np.random.random(size=(total_points, dims))
 
 
 def mu_Gaussians(p, E=np.linspace(-1, 1, 100), x0=0.5, sd=0.05):

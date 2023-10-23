6e27b008b7a0fe331397f3c5772d6858c1a78dab
Matthew Carbone <x94carbone@gmail.com>
2023-10-23 12:55:45 -0400

    Update simple sigmoid truth

--- sva/__init__.py
+++ sva/__init__.py
@@ -3,6 +3,3 @@ from dunamai import Version
 version = Version.from_any_vcs()
 __version__ = version.serialize()
 del version
-
-
-from sva.value import svf  # noqa
--- sva/truth/simple_functions.py
+++ sva/truth/simple_functions.py
@@ -1,4 +1,5 @@
 from abc import ABC, abstractmethod, abstractproperty
+from typing import Optional
 
 import numpy as np
 
@@ -6,6 +7,14 @@ import numpy as np
 class Truth(ABC):
     """Abstract base class for a source of truth. These sources of truth are
     for a single modality."""
+
+    @abstractproperty
+    def domain(self) -> Optional[np.ndarray]:
+        """The domain of valid points in the input space. The returned array
+        should be of shape (2, d), where d is the number of dimensions in the
+        input space."""
+
+        raise NotImplementedError
 
     @abstractproperty
     def n_input_dim(self) -> int:
@@ -49,6 +58,15 @@ class Truth(ABC):
         if not x.shape[1] == self.n_input_dim:
             raise ValueError(f"x second dimension must be {self.n_input_dim}")
 
+        # Ensure that the input is in the bounds
+        # Domain being None implies the domain is the entirety of the reals
+        # This is a fast way to avoid the check
+        if self.domain is not None:
+            check1 = self.domain[0, :] <= x
+            check2 = x <= self.domain[1, :]
+            if not np.all(check1 & check2):
+                raise ValueError("Some inputs x were not in the domain")
+
     def _validate_output(self, y):
 
         # Ensure y has the right shape
@@ -84,6 +102,8 @@ class SimpleSigmoid(Truth):
 
     n_input_dim = 1
     n_output_dim = 1
+    # domain = np.array([-np.inf, np.inf]).reshape(2, 1)
+    domain = None  # Equivalent to the above
 
     def __init__(self, a: float = 10.0) -> None:
         self._a = a

5586b55e91fd19076f5b3f6bd38d83d62182cd7a
Matthew Carbone <x94carbone@gmail.com>
2024-02-28 11:07:58 -0500

    Update version tracking

--- sva/__init__.py
+++ sva/__init__.py
@@ -1,5 +1,22 @@
-from dunamai import Version
+# DO NOT CHANGE BELOW ---------------------------------------------------------
+# ALSO DO NOT TYPE dunder version ANYWHERE ABOVE THIS
+# This is replaced at build time automatically during deployment and
+# installation. Replacing anything will mess that up and crash the entire
+# build.
+__version__ = "dev"  # semantic-version-placeholder
+# DO NOT CHANGE ABOVE ---------------------------------------------------------
 
-version = Version.from_any_vcs()
-__version__ = version.serialize()
-del version
+# Silly hack. Useful for local development
+if __version__ == "dev":
+    try:
+        from dunamai import Version
+
+        version = Version.from_any_vcs()
+        __version__ = version.serialize()
+        del version
+    except ImportError:
+        print("You are running a local copy of SVA (not installed via pip)")
+        print(
+            "__version__ = 'dev'; pip install dunamai to track local version"
+        )
+        pass

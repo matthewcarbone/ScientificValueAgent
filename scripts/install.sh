b3b36a2bbef10ebb1bdbcaba8548680498d24c16
Matthew Carbone <x94carbone@gmail.com>
2024-02-28 12:55:15 -0500

    Debug notebook workflow

--- scripts/install.sh
+++ scripts/install.sh
@@ -4,22 +4,23 @@
 # https://github.com/pypa/pip/issues/8049
 # This is the analog of pip install -e ".[...]" since for whatever reason
 # it does not appear to work cleanly with pip
-install_doc_requirements_only () {
-    python3 -c 'import toml; c = toml.load("pyproject.toml"); print("\n".join(c["project"]["optional-dependencies"]["doc"]))' | pip install -r /dev/stdin
+install_notebook_requirements_only() {
+	python3 -c 'import toml; c = toml.load("pyproject.toml"); print("\n".join(c["project"]["optional-dependencies"]["notebook"]))' | pip install -r /dev/stdin
 }
 
-install_test_requirements_only () {
-    python3 -c 'import toml; c = toml.load("pyproject.toml"); print("\n".join(c["project"]["optional-dependencies"]["test"]))' | pip install -r /dev/stdin
+install_test_requirements_only() {
+	python3 -c 'import toml; c = toml.load("pyproject.toml"); print("\n".join(c["project"]["optional-dependencies"]["test"]))' | pip install -r /dev/stdin
 }
 
 install_requirements() {
-    python3 -c 'import toml; c = toml.load("pyproject.toml"); print("\n".join(c["project"]["dependencies"]))' | pip install -r /dev/stdin
+	python3 -c 'import toml; c = toml.load("pyproject.toml"); print("\n".join(c["project"]["dependencies"]))' | pip install -r /dev/stdin
 }
-
 
 pip install toml
 if [ "$1" = "test" ]; then
-    install_test_requirements_only
+	install_test_requirements_only
+elif [ "$1" = "notebook" ]; then
+	install_notebook_requirements_only
 else
-    install_requirements
+	install_requirements
 fi

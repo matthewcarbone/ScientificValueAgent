48f8af16f817f406d0937527660fc0b4a86bb193
Matthew Carbone <x94carbone@gmail.com>
2024-05-03 13:00:12 -0400

    Remove most of base experiment

--- sva/experiments/base.py
+++ sva/experiments/base.py
@@ -106,52 +106,6 @@ class ExperimentData(MSONable):
         return True
 
 
-@define
-class ExperimentHistory(MSONable):
-    """Container for the history of the experiment. Note that this is not
-    MSONable as it will contain a variety of objects that can only be
-    pickled, so that is the protocol we'll use for this."""
-
-    history: list = field(factory=list)
-
-    def append(self, x):
-        assert isinstance(x, dict)
-        self.history.append(x)
-
-    def extend(self, x):
-        assert all([isinstance(xx, dict) for xx in x])
-        self.history.extend(x)
-
-    def __len__(self):
-        return len(self.history)
-
-    def __getitem__(self, ii):
-        return self.history[ii]
-
-    def __eq__(self, x):
-        if len(self.history) != len(x.history):
-            return False
-        for x1, x2 in zip(self.history, x.history):
-            if len(x1) != len(x2):
-                return False
-            if x1.keys() != x2.keys():
-                return False
-            for key in x1.keys():
-                # Not really sure how to do this comparison yet
-                if key == "state":
-                    continue
-                v1 = x1[key]
-                v2 = x2[key]
-                if key == "easy_gp":
-                    if v1 != v2:
-                        return False
-                if isinstance(v1, (np.ndarray, torch.Tensor)) and np.all(
-                    v1 != v2
-                ):
-                    return False
-        return True
-
-
 @frozen
 class ExperimentProperties(MSONable):
     """Defines the core set of experiment properties, which are frozen after
@@ -184,18 +138,12 @@ class ExperimentProperties(MSONable):
         return True
 
 
-NOISE_TYPES = (Callable, float, np.ndarray, list, type(None))
-
-
 @define
 class ExperimentMixin(ABC, MSONable):
     """Abstract base class for a source of truth. These sources of truth are
     for a single modality."""
 
-    metadata = field(factory=lambda: defaultdict(list))
-    noise = field(default=None, validator=validators.instance_of(NOISE_TYPES))
-    data = field(factory=lambda: ExperimentData())
-    history = field(factory=lambda: ExperimentHistory())
+    metadata = field(factory=dict)
 
     @classmethod
     def from_random(cls, n=3):
@@ -228,16 +176,6 @@ class ExperimentMixin(ABC, MSONable):
     @property
     def name(self):
         return self.__class__.__name__
-
-    def _variance(self, _):
-        """Vectorized truth of the variance of the experiment. Distinct from
-        the 'noise' property, this method returns the variance (one standard
-        deviation^2) on the observed results. By default, this returns None,
-        which is distinct from 0 (there's a difference between having a
-        completely precise observation, returning 0, and having no knowledge
-        of the noise, returning None)."""
-
-        return None
 
     def _dtruth(self):
         """The derivative of the truth value with respect to x. May not be
@@ -281,20 +219,13 @@ class ExperimentMixin(ABC, MSONable):
                 f"y second dimension must be {self.properties.n_output_dim}"
             )
 
-    def variance(self, x: np.ndarray, validate=False):
-        if validate:
-            self._validate_input(x)
-        return self._variance(x)
-
     def truth(self, x: np.ndarray) -> np.ndarray:
         """Access the noiseless results of an "experiment"."""
 
         self._validate_input(x)
         y = self._truth(x)
         self._validate_output(y)
-        # No need to validate x again since it's validated above
-        yvar = self._variance(x)
-        return y, yvar
+        return y
 
     def dtruth(self, x: np.ndarray) -> np.ndarray:
         """Access the derivative of the noiseless results of the experiment."""
@@ -387,113 +318,4 @@ class ExperimentMixin(ABC, MSONable):
         """The (possibly noisy) result of the experiment. Also returns the
         variance of the observations."""
 
-        y, yvar = self.truth(x)
-
-        if self.noise is None:
-            return y, yvar
-
-        if isinstance(self.noise, Callable):
-            noise = self.noise(x)
-            return y + np.random.normal(scale=noise, size=y.shape), yvar
-
-        if isinstance(self.noise, float):
-            pass
-        elif isinstance(self.noise, np.ndarray):
-            assert self.noise.ndim == 1
-            assert len(self.noise) == self.properties.n_output_dim
-        elif isinstance(self.noise, list):
-            assert len(self.noise) == self.properties.n_output_dim
-        else:
-            raise ValueError("Incompatible noise type")
-
-        return y + np.random.normal(scale=self.noise, size=y.shape), yvar
-
-
-class MultimodalExperimentMixin(ExperimentMixin):
-    @abstractmethod
-    def n_modalities(self):
-        raise NotImplementedError
-
-    def _validate_input(self, x):
-        # Ensure x has the right shape
-        if not x.ndim == 2:
-            raise ValueError(
-                "x must have shape (N, d + 1), where d is the dimension of "
-                "the feature, and the added extra dimension is the task index"
-            )
-
-        # Ensure that the second dimension of x is the right size for the
-        # chosen experiment
-        if not x.shape[1] == self.properties.n_input_dim + 1:
-            raise ValueError(
-                f"x second dimension must be {self.properties.n_input_dim+1}"
-            )
-
-        # Ensure that the input is in the bounds
-        # Domain being None implies the domain is the entirety of the reals
-        # This is a fast way to avoid the check
-        if self.properties.valid_domain is not None:
-            check1 = self.properties.valid_domain[0, :] <= x[:, :-1]
-            check2 = x[:, :-1] <= self.properties.valid_domain[1, :]
-            if not np.all(check1 & check2):
-                raise ValueError("Some inputs x were not in the domain")
-
-    def get_random_coordinates(self, n, modality=0):
-        x = super().get_random_coordinates(n)
-        n = x.shape[0]
-        modality_array = np.zeros((n, 1)) + modality
-        return np.concatenate([x, modality_array], axis=1)
-
-    def get_dense_coordinates(self, ppd, modality=0):
-        """Gets a set of dense coordinates, augmented with the modality
-        index, which defaults to 0.
-
-        Parameters
-        ----------
-        ppd : int or list
-            Points per dimension.
-        modality : int
-            Indexes the modality to use in multi-modal experiments.
-        domain : np.ndarray, torch.tensor, optional
-            The experimental domain of interest. If not provided defaults to
-            that of the self experiment.
-
-        Returns
-        -------
-        np.ndarray
-        """
-
-        x = super().get_dense_coordinates(ppd)
-        n = x.shape[0]
-        modality_array = np.zeros((n, 1)) + modality
-        return np.concatenate([x, modality_array], axis=1)
-
-    def initialize_data(self, modality=0, protocol="random", **kwargs):
-        """Initializes the X data via some provided protocol. Takes care to
-        initialize the multi-modal data with the provided modality.
-
-        Parameters
-        ----------
-        modality : int
-            The modality to use during initialization. Defaults to 0, which
-            can be assumed to be the low-fidelity experiment inforomation.
-        protocol : str, optional
-            Can be one of either "random" or "dense".
-        kwargs
-            Keyword arguments to pass to the particular "getter". For example
-            if protocol == "random", one needs to pass `n=...` (the number
-            of points to sample). If protocol == "dense", one needs to pass
-            `ppd=...` (the number of points per dimension on a dense grid).
-
-        Note
-        ----
-        You can call initialize_data multiple times using different modalities.
-        """
-
-        if protocol == "random":
-            X = self.get_random_coordinates(**kwargs, modality=modality)
-        elif protocol == "dense":
-            X = self.get_dense_coordinates(**kwargs, modality=modality)
-        else:
-            raise NotImplementedError(f"Unknown provided protocol {protocol}")
-        self.update_data(X)
+        return self.truth(x)

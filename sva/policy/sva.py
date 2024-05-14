59371272a99aa2e646c7c31db1a55756c70177fb
Matthew Carbone <x94carbone@gmail.com>
2024-05-14 12:27:08 -0400

    Debug sva policy

--- sva/policy/sva.py
+++ sva/policy/sva.py
@@ -1,14 +1,15 @@
-from attrs import field
+from attrs import define, field
 
 from sva.value import SVF
 
 from .base import FixedPolicy
 
 
+@define
 class FixedSVAPolicy(FixedPolicy):
     """Executes a Scientific Value Function-driven experiment."""
 
     svf = field(factory=SVF)
 
     def _get_data(self, experiment, data):
-        return data.X, self.svf(data.X, data.Y)
+        return data.X, self.svf(data.X, data.Y).reshape(-1, 1)

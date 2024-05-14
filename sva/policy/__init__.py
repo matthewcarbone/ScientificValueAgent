2d70f17460678f8a482da0005e65ff28e32b7c72
Matthew Carbone <x94carbone@gmail.com>
2024-05-14 10:52:06 -0400

    Update configs for Peierls and SVA

--- sva/policy/__init__.py
+++ sva/policy/__init__.py
@@ -1,4 +1,10 @@
 from .base import FixedPolicy, RandomPolicy
 from .explore_exploit.explore_exploit import SigmoidBetaAnnealingPolicy
+from .sva import FixedSVAPolicy
 
-__all__ = ["RandomPolicy", "FixedPolicy", "SigmoidBetaAnnealingPolicy"]
+__all__ = [
+    "RandomPolicy",
+    "FixedPolicy",
+    "SigmoidBetaAnnealingPolicy",
+    "FixedSVAPolicy",
+]

af02219ee6fd3d3ae253f4e5397f7ddc05e76084
Matthew Carbone <x94carbone@gmail.com>
2024-06-21 13:42:20 -0400

    Update sva for proximity penalty

--- sva/value.py
+++ sva/value.py
@@ -10,7 +10,11 @@ from scipy.spatial import distance_matrix
 from sva.monty.json import MSONable
 
 
-def svf(X, Y, sd=None, multiplier=1.0):
+def sigmoid(d, x0, a):
+    return 1.0 / (1.0 + np.exp(-(d - x0) / a))
+
+
+def svf(X, Y, sd=None, multiplier=1.0, proximity_penalty=None):
     """The value of two datasets, X and Y. Both X and Y must have the same
     number of rows. The returned result is a value of value for each of the
     data points.
@@ -27,6 +31,7 @@ def svf(X, Y, sd=None, multiplier=1.0):
     multiplier : float, optional
         Multiplies the automatically derived length scale if ``sd`` is
         ``None``.
+    proximity_penalty : float, optional
 
     Returns
     -------
@@ -44,45 +49,20 @@ def svf(X, Y, sd=None, multiplier=1.0):
 
     Y_dist = distance_matrix(Y, Y)
 
-    v = Y_dist * np.exp(-(X_dist**2) / sd**2)  # Removed factor of 2
+    v = Y_dist * np.exp(-X_dist / sd)  # Removed factor of 2
+
+    if proximity_penalty is not None:
+        l_max = distance.min(axis=1).max()
+        v = v * sigmoid(X_dist, l_max / proximity_penalty, 0.05)
 
     return v.mean(axis=1)
-
-
-def global_penalty(X):
-    X_dist = distance_matrix(X, X)
-    X_dist[X_dist == 0] = np.inf
-    h_min = X_dist.min(axis=1).reshape(-1, 1)
-    h_max = h_min.max()  # Maximum nearest neighbor distance
-
-    # Trying exponential decay
-    # return (1.0 - np.exp(-h_min / h_max)).squeeze()
-
-    # Trying sidmoid
-    return (1.0 / (1.0 + np.exp(-(h_min - h_max) / 1.0))).squeeze()
-
-
-def svf_global_penalty(X, v_i):
-    """Takes the result of the standard scientific value function and applies
-    a global penalty on the points: each point x_i, constraining it such that
-    the value will be low if that point it too close to neighboring points.
-    The length scale of this penalty is set by looking at the largest
-    distance in the dataset."""
-
-    return global_penalty(X) * v_i
 
 
 @define
 class SVF(MSONable):
     sd = field(default=None)
     multiplier = field(default=1.0)
+    proximity_penalty = field(default=None)
 
     def __call__(self, X, Y):
-        return svf(X, Y, self.sd, self.multiplier)
-
-
-@define
-class SVFGlobalPenalty(SVF):
-    def __call__(self, X, Y):
-        v_i = svf(X, Y, self.sd, self.multiplier)
-        return svf_global_penalty(X, v_i)
+        return svf(X, Y, self.sd, self.multiplier, self.proximity_penalty)

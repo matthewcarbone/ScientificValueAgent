4f10715708c37420bbdc9f26ed5c4ace409782de
Matthew Carbone <x94carbone@gmail.com>
2024-11-26 13:15:50 -0500

    Update versioning, packaging with uv, and add uv lockfile

--- sva/_version.py
+++ sva/_version.py
@@ -1,18 +1,1 @@
-from warnings import warn
-
-try:
-    import dunamai as _dunamai
-
-    __version__ = _dunamai.Version.from_any_vcs().serialize()
-    del _dunamai
-except ImportError:
-    warn(
-        "You are using a local development copy of sva,"
-        "dunamai is not installed, as such __version__ cannot be"
-        "detected and is set to 'dev' by default. For correct local"
-        "version tracking, install dunamai locally (pip install dunamai)."
-    )
-    __version__ = "dev"
-except Exception as err:
-    warn(f"Unknown exception during versioning: {err}. Version set to 'dev'")
-    __version__ = "dev"
+__version__ = '1.1.0.post230.dev0+414a4ae'
